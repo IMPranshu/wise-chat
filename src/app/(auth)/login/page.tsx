947e7b641d58a8946acb983f3d00df016dc7ddae
Pranshu Kumar Agrawal <pranshukumar1999@gmail.com>
2023-07-02 20:13:42 +0530

    add logo in login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Icon, Icons } from "@/components/Icons";
 import Button from "@/components/ui/Button";
 import { FC, useState } from "react";
 import { signIn } from "next-auth/react";
@@ -26,7 +27,7 @@ const page: FC<pageProps> = ({}) => {
       <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="w-full flex flex-col items-center max-w-md space-y-8">
           <div className="flex flex-col items-center gap-8">
-            logo
+            <Icons.Logo className="h-8 w-auto text-indigo-600" />
             <h2 className="mt-6 text-center text-3xl font-bole tracking-tight text-gray-900">
               Sign in to your account
             </h2>
@@ -34,7 +35,7 @@ const page: FC<pageProps> = ({}) => {
           <Button
             isLoading={isLoading}
             type="button"
-            className="max-w-sm mx-auto w-full"
+            className="max-w-sm mx-auto w-full bg-black"
             onClick={loginWithGoogle}
           >
             {isLoading ? null : (
